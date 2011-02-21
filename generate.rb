0c1725b2603ce180d1cc5f8d264a9ebe0c11d2a0
Jørgen P. Tjernø <jorgenpt@gmail.com>
2011-02-20 18:16:23 -0800

    Fixed broken resizing. Made header/footer better. Working slideshow.

--- generate.rb
+++ generate.rb
@@ -10,10 +10,11 @@ require 'haml'
 
 include Magick
 
-SMALL_SIZE = '320x256'
-MEDIUM_SIZE = '800x600'
-LARGE_SIZE = '1280x1024'
+SMALL_SIZE = [320, 256]
+MEDIUM_SIZE = [800, 600]
+LARGE_SIZE = [1280, 1024]
 EXIF_DATE_FORMAT = '%Y:%m:%d %H:%M:%S'
+ROOT = '..'
 
 class Template
     @@cache = {}
@@ -23,19 +24,19 @@ class Template
             @@cache[file] = Haml::Engine.new(File.read(file))
         end
 
-        @SMALL_SIZE = SMALL_SIZE
-        @MEDIUM_SIZE = MEDIUM_SIZE
-        @LARGE_SIZE = LARGE_SIZE
-
         @engine = @@cache[file]
+        @locals = {}
     end
 
     def render(locals={})
+        @locals = locals
         @engine.render(self, locals)
     end
-end
 
-footer = Template.new('footer.haml').render
+    def include_template(file, locals={})
+        Template.new("#{file}.haml").render(locals.merge(@locals))
+    end
+end
 
 albums = Hash.new { |hash, key| hash[key] = [] }
 Dir.new('.').each { |album|
@@ -85,22 +86,23 @@ Dir.new('.').each { |album|
         large_filename = "#{output_large}/#{entry}"
 
         image = Image.read(filename).first.auto_orient
+        image.sync_profiles
 
         taken = image.get_exif_by_entry('DateTime').first[1]
         taken = Date.strptime(taken, EXIF_DATE_FORMAT)
 
         if not File.exist? large_filename then
-            image.resize_to_fit!(LARGE_SIZE)
+            image.resize_to_fit!(*LARGE_SIZE)
             image.write(large_filename)
         end
 
         if not File.exist? medium_filename then
-            medium_image = image.resize_to_fit(MEDIUM_SIZE)
+            medium_image = image.resize_to_fit(*MEDIUM_SIZE)
             medium_image.write(medium_filename)
         end
 
         if not File.exist? small_filename then
-            small_image = image.resize_to_fit(SMALL_SIZE)
+            small_image = image.resize_to_fit(*SMALL_SIZE)
             small_image.write(small_filename)
         else
             small_image = Image.ping(small_filename).first
@@ -129,7 +131,7 @@ Dir.new('.').each { |album|
         album_content << Template.new('album.per_day.haml').render({:date => Date.strptime(day), :images => images_html})
     }
 
-    album_html = Template.new('album.haml').render({:title => info['title'], :images => album_content.join("\n"), :footer => footer})
+    album_html = Template.new('album.haml').render({:title => info['title'], :images => album_content.join("\n")})
     File.open(output_html, 'w') { |f| f.write(album_html) }
 
     range_start = first_taken.strftime('%e')
@@ -157,5 +159,5 @@ albums.each { |year, album_list|
     years_content << index_template.render({:year => year, :albums => albums_content})
 }
 
-index_content = Template.new('index.haml').render({:title => "bilder.o7.no", :albums => years_content.join("\n"), :footer => footer})
+index_content = Template.new('index.haml').render({:title => "bilder.o7.no", :albums => years_content.join("\n")})
 File.open("output/index.html", 'w') { |f| f.write(index_content) }
